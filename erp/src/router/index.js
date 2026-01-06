e9301d4f997f1f04a2b823384f25d4255c1ff8fe
Solutions Quasar <benjamin@solutionsquasar.ca>
2026-01-06 13:00:58 -0400

    fix(router): use base url for history mode

--- erp/src/router/index.js
+++ erp/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../pages/Dashboard.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
